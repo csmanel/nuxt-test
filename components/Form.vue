89c33b49e8e5c281674b1e9ba685da799ba418e6
Cole Manel <csmanel@gmail.com>
2024-12-18 20:39:49 -0500

    custom validation for form

--- components/Form.vue
+++ components/Form.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts">
+<!-- basic form -->
+
+<!-- <script setup lang="ts">
 import { object, string, type InferType } from 'yup';
 import type { FormSubmitEvent } from '#ui/types';
 
@@ -23,6 +25,47 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 
 <template>
   <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password">
+      <UInput v-model="state.password" type="password" />
+    </UFormGroup>
+
+    <UButton type="submit"> Submit </UButton>
+  </UForm>
+</template> -->
+
+<!-- custom validation -->
+
+<script setup lang="ts">
+import type { FormError, FormSubmitEvent } from '#ui/types';
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+const validate = (state: any): FormError[] => {
+  const errors = [];
+  if (!state.email) errors.push({ path: 'email', message: 'Required' });
+  if (!state.password) errors.push({ path: 'password', message: 'Required' });
+  return errors;
+};
+
+async function onSubmit(event: FormSubmitEvent<any>) {
+  console.log(event.data);
+}
+</script>
+
+<template>
+  <UForm
+    :validate="validate"
+    :state="state"
+    class="space-y-4"
+    @submit="onSubmit"
+  >
     <UFormGroup label="Email" name="email">
       <UInput v-model="state.email" />
     </UFormGroup>

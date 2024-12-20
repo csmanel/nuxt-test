659a077e0488c546d201b288976ee953f5464e5f
Cole Manel <csmanel@gmail.com>
2024-12-19 20:10:51 -0500

    form seterrors function

--- components/Form.vue
+++ components/Form.vue
@@ -39,7 +39,7 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 
 <!-- custom validation -->
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import type { FormError, FormSubmitEvent } from '#ui/types';
 
 const state = reactive({
@@ -66,6 +66,53 @@ async function onSubmit(event: FormSubmitEvent<any>) {
     class="space-y-4"
     @submit="onSubmit"
   >
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password">
+      <UInput v-model="state.password" type="password" />
+    </UFormGroup>
+
+    <UButton type="submit"> Submit </UButton>
+  </UForm>
+</template> -->
+
+<script setup lang="ts">
+import type { Form, FormSubmitEvent } from '#ui/types';
+
+interface Schema {
+  email?: string;
+  password?: string;
+}
+
+const state = reactive<Schema>({
+  email: undefined,
+  password: undefined,
+});
+
+const form = ref<Form<Schema>>();
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  form.value!.clear();
+  try {
+    const response = await $fetch('...');
+    // ...
+  } catch (err) {
+    if (err.statusCode === 422) {
+      form.value!.setErrors(
+        err.data.errors.map((err) => ({
+          message: err.message,
+          path: err.path,
+        }))
+      );
+    }
+  }
+}
+</script>
+
+<template>
+  <UForm ref="form" :state="state" @submit="onSubmit">
     <UFormGroup label="Email" name="email">
       <UInput v-model="state.email" />
     </UFormGroup>

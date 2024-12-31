8eead8a216a057631fac013fed46902f59f8de3b
Cole Manel <csmanel@gmail.com>
2024-12-31 16:59:38 -0500

    add to app theme, use accordian, and dropdown

--- app.vue
+++ app.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'Getting Started',
+    icon: 'i-heroicons-information-circle',
+    defaultOpen: true,
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.',
+  },
+  {
+    label: 'Installation',
+    icon: 'i-heroicons-arrow-down-tray',
+    disabled: true,
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.',
+  },
+];
+</script>
+
 <template>
   <UContainer>
     <UCard class="mt-10">
       <template #header>
+        <UDropdown :items="[[{ label: 'Edit', shortcuts: ['E'] }]]" />
+        <UAccordion color="primary" :items="items" />
+
         <div class="flex justify-between">
           <h1>Welcome to Nuxt UI Starter</h1>
           <ColorScheme

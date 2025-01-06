6976316270b9ee9ca24d80aef72066a8af877049
Cole Manel <csmanel@gmail.com>
2025-01-06 18:29:38 -0500

    defuault layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt"],
-  compatibilityDate: "2024-12-17"
-})
+  modules: ['@nuxt/ui', '@vueuse/nuxt'],
+  compatibilityDate: '2024-12-17',
+  colorMode: {
+    classSuffix: '',
+  },
+});

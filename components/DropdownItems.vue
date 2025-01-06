09a790767959d25f309bafa3457a4f77c6e09885
Cole Manel <csmanel@gmail.com>
2025-01-06 18:30:26 -0500

    getting started default open drawer set to false

--- components/DropdownItems.vue
+++ components/DropdownItems.vue
@@ -3,7 +3,7 @@ const items = [
   {
     label: 'Getting Started',
     icon: 'i-heroicons-information-circle',
-    defaultOpen: true,
+    defaultOpen: false,
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.',
   },

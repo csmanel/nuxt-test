5d440b71f6d0184f48b227ac825698df67615d81
Cole Manel <csmanel@gmail.com>
2025-01-14 12:43:52 -0500

    setup basic order modal, cart, and menu items array

---
+++ components/MenuItems/menuCart.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+const cart = ref<string[]>([]);
+const showModal = ref(false);
+
+const addToCart = (item: string) => {
+  if (!cart.value.includes(item)) {
+    cart.value.push(item);
+  }
+};
+
+const placeOrder = () => {
+  showModal.value = true;
+};
+</script>
---
+++ components/MenuItems/menuItems.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const menuItems = ref([{
+  {
+    group: 'Pour Over',
+    options: ['Columbian', 'Ethiopian', 'Rwanda'],
+  },
+  {
+    group: 'Espresso',
+    options: ['Latte', 'Americano', 'Double Espresso'],
+  },
+  {
+    group: 'Teas',
+    options: ['Green Tea', 'Black Tea', 'Oolong'],
+  },
+}]);
+</script>
